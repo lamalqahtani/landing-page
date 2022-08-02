01435517314d2d2e7e6079385b0c664b164516c4
Lamyaa Q <lamiaa@Lamyaas-MacBook-Pro.local>
2022-08-02 16:21:33 +0300

    edit app.js: global variables declaration and set active section function added

--- js/app.js
+++ js/app.js
@@ -22,6 +22,11 @@
  * Define Global Variables
  * 
 */
+const fragment = document.createDocumentFragment();
+const sections = document.querySelectorAll('section');
+let list = document.querySelector('#navbar__list');
+const viewportWidth20 = (window.innerHeight*20)/100;
+
 
 
 /**
@@ -30,6 +35,18 @@
  * 
 */
 
+// 1.loop inside the list of sections
+// 2.when user scrolls check the position of the sections one bu one to look for the one the user is at.
+function setActiveSection(){
+    for(let i = 0; i< sections.length; i++){
+        console.log(` section ${i} : ${sections[i].getBoundingClientRect().top}`);
+        if(sections[i].getBoundingClientRect().top >= viewportWidth20){
+            sections[i].setAttribute('class','your-active-class');
+        }else{
+            sections[i].setAttribute('class','nothing');
+        }
+    }
+}
 
 
 /**
@@ -39,12 +56,21 @@
 */
 
 // build the nav
-
+    // 1.create list elements 
+    for(let i = 0; i < sections.length; i++){
+        let listItem = document.createElement('li');
+        listItem.textContent = sections[i].getAttribute('data-nav');
+        listItem.setAttribute('class','menu__link');
+        fragment.appendChild(listItem);
+    }
+    // 2.appent list elements to ul using fragment
+    list.appendChild(fragment);
 
 // Add class 'active' to section when near top of viewport
-
+document.addEventListener('scroll', setActiveSection );
 
 // Scroll to anchor ID using scrollTO event
+
 
 
 /**
@@ -58,5 +84,16 @@
 // Scroll to section on link click
 
 // Set sections as active
+
+
+
+
+
+
+
+//tests
+    // document.addEventListener('click',()=>{
+    //     sections[2].scrollIntoView();
+    // })
 
 
